307f07516c88c298ff891bf210d48aaf371a1279
Sancher Li <SancherLi96@gmail.com>
2026-08-28 18:00:54 +0500

    static css, featured app

--- main/views.py
+++ main/views.py
@@ -1,13 +1,17 @@
 from django.apps.registry import Apps
 from django.shortcuts import render
-
+from pyexpat import features
 
 from .models import App
 
 
 def index(request):
-    apps = App.objects.all()
-    return render(request, 'main/index.html', {'apps': apps})
+    apps = App.objects.order_by('created_at').all()
+    features = App.objects.order_by('-price').first()
+    return render(request, 'main/index.html', {
+        'apps': apps,
+        'features': features,
+    })
 
 
 def about(request):

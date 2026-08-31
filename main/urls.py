3ffb7ed37417d50f2778f6b8a10d4a9b52bbc38c
Sancher Li <SancherLi96@gmail.com>
2026-08-31 19:45:44 +0500

    create page new

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-
 from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('about/',views.about, name='about'),
     path('app/<int:app_id>',views.app_detail, name='app detail'),
-    path('category/<int:category_id>/',views.category_detail, name='category detail'),
+    path('category/<int:category_id>/',views.category_detail, name='category'),
+    path('new/', views.new, name='new'),
 ]
